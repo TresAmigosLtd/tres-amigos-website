f0b81c113dae2699a343cee76f1bc851985991c1
Benjamin Grohbiel <ben.grohbiel@snyk.io>
2022-07-16 09:09:19 +0100

    Experiment with copy

--- components/hero.tsx
+++ components/hero.tsx
@@ -9,10 +9,10 @@ export default function Hero() {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              Accelerate your engineering team.
+              We are your engineering team.
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-              We build, scale and run high-performing engineering teams, that make businesses succeed.
+              Three software engineers ready to build, scale and run high-performing engineering teams, to make businesses succeed.
             </p>
 
             {/*<div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">*/}
