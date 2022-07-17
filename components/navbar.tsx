d5917a7746c903ee6eb1f85411541f8c89e3a4e7
José Carlos Valero Sánchez <jose.valero@snyk.io>
2022-07-17 11:36:54 +0100

    feat: work on navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
             <>
               <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
                 <Link href="/">
-                  <a className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
+                  <a className="flex items-center space-x-2 text-2xl font-medium text-gray-800 dark:text-gray-100">
                     <span>Tres Amigos</span>
                   </a>
                 </Link>
@@ -48,10 +48,10 @@ export default function Navbar() {
                 <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
                   <>
                     {navigation.map((item, index) => (
-                      <Link key={index} href="/">
-                        <a className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 dark:focus:bg-gray-800 focus:outline-none dark:focus:bg-trueGray-700">
-                          {item}
-                        </a>
+                      <Link key={index} href={item.scrollTo}>
+                          <a className="w-full px-4 py-2 -ml-4">
+                            {item.label}
+                          </a>
                       </Link>
                     ))}
                   </>
@@ -65,9 +65,9 @@ export default function Navbar() {
         <div className="hidden text-center lg:flex lg:items-center">
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
             {navigation.map((menu, index) => (
-              <li className="mr-3 nav__item" key={index}>
+              <li className="navbar__item" key={index}>
                 <Link href={menu.scrollTo}>
-                  <a className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
+                  <a className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline dark:text-gray-200 focus:outline-none ">
                     {menu.label}
                   </a>
                 </Link>
